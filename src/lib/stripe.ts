270336adea060e548516e9c99380b9113c7335a5
s4kuraN4gi <main2acount.mt@gmail.com>
2025-12-23 13:49:32 +0900

    fix(stripe): Update API version to 2025-12-15.clover

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
-  apiVersion: '2024-12-18.acacia', // Use latest API version or pin to what you need
+  apiVersion: '2025-12-15.clover', // Use latest API version or pin to what you need
   appInfo: {
     name: 'LoL Coach AI',
     version: '0.1.0',
